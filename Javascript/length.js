e9355cd7fb44278bae33f8ca1101d0250c29ff1e
1611Aryan <agupta32_be19@thapar.edu>
2020-09-07 15:29:30 +0530

    init

---
+++ Javascript/length.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+var progress = document.getElementById('progressBar');
+var totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function(){
+  var progressHeight = (window.pageYOffset / totalHeight) * 100;
+  progress.style.height = progressHeight + "%";
+}
